7c83a50c8fc5fb7c5fa4a10547696eec9cd6bdb6
mainaliashish <mainaliashish@outlook.com>
2024-10-31 14:55:13 +0000

    update: - setup styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import "./App.css";
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  color: rebeccapurple;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+`;
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <H1>The Wild Oasis</H1>
+      <Button onClick={() => alert("Hello")}>Check in</Button>
+    </div>
+  );
 }
 
 export default App;
